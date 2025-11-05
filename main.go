e218a0df47129e40d9909590e4e528938d1e4e17
StrikerReaper4 <strikerreaper4@gmail.com>
2025-11-04 22:12:11 -0400

    Update main.go

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+
 	"github.com/rs/cors"
 )
 
@@ -14,34 +15,28 @@ func main() {
 	defer config.DB.Close()
 
 	http.HandleFunc("/", controller.Handler)
-
 	http.HandleFunc("/criar/usuario", controller.Create)
-
 	http.HandleFunc("/login/usuario", controller.Login)
-
 	http.HandleFunc("/criar/imovel", controller.CreateImovel)
-
 	http.HandleFunc("/filtrar/imoveis", controller.FilterImovel)
-
 	http.HandleFunc("/deletar/imovel", controller.DeleteImovel)
-
 	http.HandleFunc("/atualizar/imovel", controller.UpdateImovel)
 
 	c := cors.New(cors.Options{
-		AllowedOrigins:   []string{"http://localhost:5173","https://frontend-imovel-bretas.onrender.com"},
+		AllowedOrigins:   []string{"http://localhost:5173", "https://frontend-imovel-bretas.onrender.com"},
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders:   []string{"Content-Type", "Authorization"},
 		AllowCredentials: true,
 	})
 
 	handler := c.Handler(http.DefaultServeMux)
-	
+
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "8080" // fallback para rodar localmente
+		port = "8080"
 	}
-	
-	log.Println("Servidor rodando na porta %s...", port)
-	log.Fatal(http.ListenAndServe(":8080", handler))
 
+	log.Printf("Servidor rodando na porta %s...", port)
+
+	log.Fatal(http.ListenAndServe(":"+port, handler))
 }
